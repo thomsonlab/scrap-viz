57482acf537754f9d9235651fd98a4cdcd2dfb85
dibidave <dibidave@gmail.com>
2019-01-13 23:58:52 -0800

    Updated h5 conversion to work with cellranger 3

--- convert_h5_to_csv.py
+++ convert_h5_to_csv.py
@@ -5,19 +5,25 @@ import pandas
 import numpy
 from scRNA_seq import Gene_Expression_Dataset
 
-workspace_path = '/media/dibidave/mobinoodle/Virus_Transcriptomics/DRG'
-transcript_file_path = os.path.join(workspace_path, "transcripts", "filtered_gene_bc_matrices_h5.h5")
+workspace_path = os.getcwd()
+transcript_file_path = os.path.join(workspace_path, "transcripts", "filtered_feature_bc_matrix.h5")
 
 hdf = h5py.File(transcript_file_path)
 
 sparse = csc_matrix(
-    (hdf["mm10"]["data"], hdf["mm10"]["indices"], hdf["mm10"]["indptr"]))
+    (hdf["matrix"]["data"], hdf["matrix"]["indices"], hdf["matrix"]["indptr"]))
 
 df = pandas.SparseDataFrame(sparse, dtype=numpy.uint32)
 df = df.to_dense()
 df = df.fillna(0)
 df = df.astype(int)
-df.index = [x.decode("UTF-8") for x in list(hdf["mm10"]["gene_names"])]
-df.columns = [x.decode("UTF-8") for x in list(hdf["mm10"]["barcodes"])]
+num_genes_present = df.shape[0]
+num_genes = len(list(hdf["matrix"]["features"]))
+gene_names = [x.decode("UTF-8") for x in list(hdf["matrix"]["features"]["name"])]
+barcodes = [x.decode("UTF-8") for x in list(hdf["matrix"]["barcodes"])]
+df.index = gene_names[0:num_genes_present]
+df.columns = barcodes
+if num_genes != num_genes_present:
+    df = df.append(pandas.DataFrame(0, index=gene_names[num_genes_present:], columns=barcodes))
 Gene_Expression_Dataset.write_pandas_csv(
     df, os.path.join(workspace_path, "transcripts", "gene_counts.csv"))
